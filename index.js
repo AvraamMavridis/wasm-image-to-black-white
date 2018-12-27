ce574b9210bb4c29884febbdc7498be0265fb38a
AvraamMavridis <avraammauridis@gmail.com>
2018-12-27 20:25:36 +0100

    Demo

--- index.js
+++ index.js
@@ -3,27 +3,25 @@ const wasm = import("wasm-image-to-black-white");
 const fileUploader = document.querySelector("#uploadfile");
 const results = document.querySelector("#results");
 
+console.log(fileUploader, results);
+
 fileUploader.addEventListener('change', (event) => {
   const file = event.target.files[0];
   results.style.opacity = 1;
-
-  console.log(file);
 
   var img = document.querySelector("#original");
 
   img.onload = function() {
     wasm.then(bnw => {
-      console.log(bnw);
-
       const withLum = document.querySelector("#luminocity");
       withLum.src = bnw.grayscale_with_luminocity(img);
-      withLum.style.width = img.width;
-      withLum.style.height = img.height;
+      withLum.width = img.width;
+      withLum.height = img.height;
 
       const avg = document.querySelector("#average");
       avg.src = bnw.grayscale_with_average(img);
-      avg.style.width = img.width;
-      avg.style.height = img.height;
+      avg.width = img.width;
+      avg.height = img.height;
     }).catch(console.error);
   }
 
